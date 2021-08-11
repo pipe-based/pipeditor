e914a90163756f3428957366be47c53d503a7194
pipe <357681911qq@gmail.com>
2021-08-11 15:55:54 +0800

    node with head and body

---
+++ src/pipe/p_node/pipe.jsx
@@ -0,0 +1,13 @@
+import './pipe.css'
+const pNode = () => {
+    return ( 
+        <div className="node-border">
+            <h1>node title</h1>
+            <div>
+                <span>node conent</span>
+            </div>
+        </div> 
+        );
+}
+ 
+export default pNode;
---
+++ src/pipe/pipe.jsx
@@ -0,0 +1,12 @@
+import Pnode from "./p_node/pipe";
+const Pipe = () => {
+    return ( 
+    <div>
+        {/* pipeditor: pipe-based editor */}
+        <Pnode></Pnode>
+        <Pnode></Pnode>
+    </div> 
+    );
+}
+ 
+export default Pipe;
